4dd4f4d03de5b6689c108132e481b20dc802a903
Vlada Lysnko <sunny.vladi@gmail.com>
2021-03-02 17:38:15 +0100

    almost done without bem

---
+++ products-coffee.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('[coffee-modal-open]'),
+        closeModalBtn: document.querySelector('[coffee-modal-close]'),
+        modal: document.querySelector('[coffee-modal]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('hidden');
+    }
+})();
---
+++ products-milkshake.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('[milkshake-modal-open]'),
+        closeModalBtn: document.querySelector('[milkshake-modal-close]'),
+        modal: document.querySelector('[milkshake-modal]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+})();
